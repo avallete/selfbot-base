0103989eac645be6937e671a7077acd576d031d7
Logan Heinzelman <logan@juicebot.xyz>
2017-07-10 08:54:39 +0000

    Moved embedColor

--- config.js
+++ config.js
@@ -4,7 +4,6 @@ module.exports = {
         token:"",
         prefix: "s!",
         ownerID: "",
-        embedColor: parseInt("0x" + Math.floor(Math.random() * 16777215).toString(16)), // I suggest not to edit this unles you know what your doing
         version: '1.0.0',
         }
     ]
