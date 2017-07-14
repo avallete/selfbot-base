8f44c3d0fec4178608432381fa7fcbebcb62ab62
Logan Heinzelman <logan@juicebot.xyz>
2017-07-14 22:27:20 +0000

    Added the embed methods to the command so it works

--- commands/Core/info.js
+++ commands/Core/info.js
@@ -1,11 +1,12 @@
 exports.run = async (client, msg) => {
+  const embed = new client.methods.Embed()
   .setTitle("Information")
   .setColor(global.embedColor)
   .addField("Developer","Loganrose#2582 (<@145749360515219456>)", false)
   .addField("About",`The selfbot your using is v${global.version}. It is built off of the Komada framework with NodeJS.`, false)
-  .addField("https://discord.gg/WkXuhbb", "Join our support server!",false)
+  .addField("https://discord.gg/WkXuhbb", "Join our support server!",false);
   embed.setFooter(`Selfbot v${global.version} | ${new Date()}`);
-  msg.channel.sendEmbed(embed)
+  msg.channel.sendEmbed(embed);
 };
 
 exports.conf = {
