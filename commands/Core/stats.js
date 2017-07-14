1890a27eb87bb911b72cce5b13d4e7d1266b679b
Logan Heinzelman <logan@juicebot.xyz>
2017-07-14 22:28:17 +0000

    Now works!

--- commands/Core/stats.js
+++ commands/Core/stats.js
@@ -4,9 +4,9 @@ const { version: komadaVersion } = require("komada");
 
 exports.run = async (client, msg) => {
   const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
-  const embed = new client.methods.Embed()
+  const embed = new client.methods.Embed();
   embed.setTitle("Statistics");
-  embed.setColor(global.embedColor)
+  embed.setColor(global.embedColor);
   embed.setAuthor('Selfbot Stats', client.user.avatarURL({format: 'png', size: 128}), 'https://www.selfbot.loganrose.xyz')
   .addField("Mem Usage", `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`,true)
   .addField("Uptime", `${duration}`,true)
